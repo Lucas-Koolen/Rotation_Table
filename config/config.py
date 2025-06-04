f15b26bc2afeaa7482fb6d32d960ff91b7ef5e2b
Lucas Koolen <59663896+Lucas-Koolen@users.noreply.github.com>
2025-06-04 11:04:43 +0200

    Adjust camera frame resolution

--- config/config.py
+++ config/config.py
@@ -4,6 +4,16 @@
 SERIAL_PORT = 'COM7'
 BAUD_RATE = 9600
 
+# Camera-instellingen
+# Pas de resolutie aan een 4:3 formaat. Indien een andere
+# cameraresolutie wordt gebruikt moet ook MM_PER_PIXEL opnieuw
+# worden bepaald voor correcte metingen.
+FRAME_WIDTH = 1600
+FRAME_HEIGHT = 1200
+PIXEL_FORMAT = 0  # standaardwaarde, indien nodig aanpassen
+EXPOSURE_TIME = 5000.0
+GAIN = 0.0
+
 # MySQL database instellingen (productiegebruik)
 DB_CONFIG = {
     'host': 'mysql.kvdelsen.nl',
